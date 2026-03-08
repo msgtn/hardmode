6f3db9799c2c5fc697efb8c25875179cfcf3a421
Shenai Chan <73866558+shenaichan@users.noreply.github.com>
2026-03-07 20:02:22 -0500

    Populated data; made server more resilient

--- backend/app.py
+++ backend/app.py
@@ -70,4 +70,4 @@ def on_message(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True, allow_unsafe_werkzeug=True)
